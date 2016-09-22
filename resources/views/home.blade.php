cb5056da2f57ec08ddc11194aae81273015bf1d4
paihuangpian <2451678579@qq.com>
2016-09-22 18:38:34 +0800

    '初始化项目'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravelstack - 专注全栈框架 Laravel 的中文视频教程网站</title>
+
+	<meta name="author" content="织席贩履之辈">
+	<meta name="description" content="Laravelstack - 专注PHP的全栈中文视频教程网站。">
+	<meta name="keywords" content="taylor otwell, laravel, laravel 教程, laravel 视频, laravel 视频教程, 织席贩履之辈">
+
+    <!-- Fonts -->
+    <link href="//cdn.bootcss.com/normalize/4.2.0/normalize.min.css" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'> -->
+	<link rel="stylesheet" href="/css/home.css">
+
+</head>
+<body>
+	<!-- header -->
+	<div class="header">
+		<div class="container">
+			<div class="brand">
+				<a href="">Laravelstack</a>
+			</div>
+			<div class="nav">
+				<a href="">教程</a>
+				<a href="">关于</a>
+				<a href="">来挖我</a>
+				<a href="">登录</a>
+			</div>
+		</div>
+	</div>
+
+	<!-- banner -->
+	<div class="container banner">
+		<h1>现代PHP</h1>
+	</div>
+</body>
+</html>
