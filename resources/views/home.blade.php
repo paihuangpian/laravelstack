f82f603f4cefc30a8ceba25b0b9c8138404ef345
paihuangpian <2451678579@qq.com>
2016-09-24 02:49:38 +0800

    '首页基本确定风格'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
 	<meta charset="UTF-8">
 	<title>Laravelstack - 专注全栈框架 Laravel 的中文视频教程网站</title>
 
-	<meta name="author" content="织席贩履之辈">
-	<meta name="description" content="Laravelstack - 专注PHP的全栈中文视频教程网站。">
+	<meta name="author" content="taylor otwell, 织席贩履之辈">
+	<meta name="description" content="Laravelstack - 专注全栈框架 Laravel 的中文视频教程网站。">
 	<meta name="keywords" content="taylor otwell, laravel, laravel 教程, laravel 视频, laravel 视频教程, 织席贩履之辈">
 
     <!-- Fonts -->
     <link href="//cdn.bootcss.com/normalize/4.2.0/normalize.min.css" rel="stylesheet">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'> -->
-	<link rel="stylesheet" href="/css/home.css">
+	<link href="/css/home.css" rel="stylesheet" type="text/css">
+	<link href="/css/lato.css" rel="stylesheet" type="text/css">
 
 </head>
 <body>
@@ -22,17 +22,85 @@
 				<a href="">Laravelstack</a>
 			</div>
 			<div class="nav">
+				<a href="" class="active">首页</a>
 				<a href="">教程</a>
 				<a href="">关于</a>
 				<a href="">来挖我</a>
-				<a href="">登录</a>
 			</div>
 		</div>
 	</div>
 
-	<!-- banner -->
-	<div class="container banner">
-		<h1>现代PHP</h1>
+	<!-- colors -->
+	<!-- <div class="container">
+		<div class="colors">
+			<span style="color: #0DCBFF;">#0DCBFF;</span>
+			<span style="color: #46acdb;">#46acdb;</span>
+			<span style="color: #546E7A;">#546E7A;</span>
+			<span style="color: #EC5F67;">#EC5F67;</span>
+			<span style="color: #CCA898;">#CCA898;</span>
+			<span style="color: #3DFF7D;">#3DFF7D;</span>
+			<span style="color: #D5EBB3;">#D5EBB3;</span>
+			<span style="color: #E8BF7E;">#E8BF7E;</span>
+			<span style="color: #99C794;">#99C794;</span>
+		</div>
+	</div> -->
+	
+	<!-- box -->
+	<div class="container">
+
+		<div class="box">
+			<h1>现代PHP工程师之路 <a href="">?</a></h1>
+			<p><b>这些是必学的</b></p>
+			<p>
+				<small>前端:</small>
+				<ul>
+					<li><a href="">Html</a></li>
+					<li><a href="">Css</a></li>
+					<li><a href="">Js</a></li>
+				</ul>
+			</p>
+			<p>
+				<small>后端:</small>
+				<ul>
+					<li><a href="">Linux(Ubuntu 16)</a></li>
+					<li><a href="">Apache/Nginx</a></li>
+					<li><a href="">Mysql</a></li>
+					<li><a href="">PHP(7)</a></li>
+				</ul>
+			</p>
+			<p>
+				<small>框架(现代PHP实现):</small>
+				<ul>
+					<li><a href="">Laravel(5.3)</a></li>
+				</ul>
+			</p>
+			<p><b>更好的驾驭Web开发</b></p>
+			<p>
+				<small>代码管理:</small>
+				<ul>
+					<li><a href="">Git</a></li>
+				</ul>
+			</p>
+			<p>
+				<small>持久化数据处理</small>
+				<ul>
+					<li><a href="">Memcached</a></li>
+					<li><a href="">Redis</a></li>
+				</ul>
+			</p>
+			<p>	
+				<small>传输协议</small>
+				<ul>
+					<li><a href="">TCP/IP、HTTP</a></li>
+				</ul>
+			</p>
+			<p><b>一些实践</b></p>
+			<p>
+				<ul>
+					<li><a href="">Git</a></li>
+				</ul>
+			</p>
+		</div>
 	</div>
 </body>
 </html>
